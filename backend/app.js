efadd067800409d951633531a64ce7e61fdfddb5
fxuls <32501694+fxuls@users.noreply.github.com>
2022-03-12 16:29:15 -0500

    Add error handling

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const csurf = require("csurf");
 const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
+const { ValidationError } = require("sequelize");
 
 const routes = require("./routes");
 
@@ -21,27 +22,58 @@ app.use(express.json());
 // enable cors in development only
 if (!isProduction) app.use(cors());
 
-
 // enable helmet
 app.use(
-    helmet.crossOriginResourcePolicy({
-        policy: "cross-origin",
-    })
+  helmet.crossOriginResourcePolicy({
+    policy: "cross-origin",
+  })
 );
 
 app.use(helmet({ contentSecurityPolicty: false }));
 
 // set up csrf
-app.use(csurf({
+app.use(
+  csurf({
     cookie: {
-        secure: isProduction,
-        sameSite: isProduction && "Lax",
-        httpOnly: true,
-    }
-}));
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true,
+    },
+  })
+);
 
 // set up routes
 app.use(routes);
 
+// Route not found handler
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = ["The requested resource couldn't be found."];
+  err.status = 404;
+  next(err);
+});
+
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map((e) => e.message);
+    err.title = "Validation error";
+  }
+  next(err);
+});
+
+// Error formatter
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || "Server Error",
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack,
+  });
+});
 
 module.exports = app;
