eb8847822d6298a39668e4e8a2af5c8b8e93de8c
fxuls <32501694+fxuls@users.noreply.github.com>
2022-03-16 19:27:33 -0400

    Idk

--- backend/db/migrations/20220316232344-create-image.js
+++ backend/db/migrations/20220316232344-create-image.js
@@ -1,14 +1,14 @@
 'use strict';
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Tests', {
+    await queryInterface.createTable('Images', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      username: {
+      url: {
         type: Sequelize.STRING
       },
       createdAt: {
@@ -22,6 +22,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Tests');
+    await queryInterface.dropTable('Images');
   }
 };
