c5c24b08f01ef6cbcc402ea477d5c83705e5d520
fxuls <32501694+fxuls@users.noreply.github.com>
2022-03-16 20:04:24 -0400

    Add group seeder

---
+++ backend/db/seeders/20220316234531-groups-seed.js
@@ -0,0 +1,49 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert("Groups", [
+      {
+        organizer_id: 1,
+        name: "Awesome group!",
+        about: "This is our awesome group! We love to meetup and hang out.",
+        type: "inperson",
+        private: false,
+        preview_image_id: 1,
+        city: "San Francisco",
+        state: "Arkansas",
+      },
+      {
+        organizer_id: 1,
+        name: "Running friends",
+        about:
+          "We are a group of friends who love to go running. Come join us at our next event!",
+        type: "virtual",
+        private: false,
+        preview_image_id: 2,
+      },
+      {
+        organizer_id: 2,
+        name: "DnD Game Night",
+        about: "Our dungeons and dragons group",
+        type: "inperson",
+        private: true,
+        city: "Pittsburgh",
+        state: "Pennsylvania",
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    const Op = Sequelize.Op;
+    await queryInterface.bulkDelete(
+      "Groups",
+      {
+        name: {
+          [Op.in]: ["Awesome group!", "Running friends", "DnD Game Night"],
+        },
+      },
+      {}
+    );
+  },
+};
