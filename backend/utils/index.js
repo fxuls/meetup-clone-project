c77785c3fe968b4ed0871cccf1be2a83db0db284
fxuls <32501694+fxuls@users.noreply.github.com>
2022-03-24 19:59:37 -0400

    Remove debugger

--- backend/utils/index.js
+++ backend/utils/index.js
@@ -1,6 +1,5 @@
 const previewImageToUrl = (model) => {
   const newModel = model.toJSON();
-  debugger;
   delete newModel.previewImageId;
   if (newModel.previewImage) {
     newModel.previewImage = model.previewImage.url;
