46ba69c1be594e2871a0990472c56c430d1b638a
fxuls <32501694+fxuls@users.noreply.github.com>
2022-03-24 18:22:21 -0400

    Create events router

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,10 +2,12 @@ const router = require("express").Router();
 const sessionRouter = require("./session");
 const usersRouter = require("./users");
 const groupsRouter = require("./groups");
+const eventsRouter = require("./events");
 
 router.use("/session", sessionRouter);
 router.use("/users", usersRouter);
 router.use("/groups", groupsRouter);
+router.use(eventsRouter);
 
 router.post("/test", function (req, res) {
   res.json({ requestBody: req.body });
