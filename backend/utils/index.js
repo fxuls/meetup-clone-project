1fd1e49ca2c553e408ba35b0fa56679964b6c5a3
fxuls <32501694+fxuls@users.noreply.github.com>
2022-03-24 19:39:21 -0400

    Add imagesToUrls This helper function converts an array of Image models into an array of their urls

--- backend/utils/index.js
+++ backend/utils/index.js
@@ -1,11 +1,15 @@
 const previewImageToUrl = (model) => {
-    const newModel = model.toJSON();
-    debugger;
-    delete newModel.previewImageId;
-    if (newModel.previewImage) {
-      newModel.previewImage = model.previewImage.url;
-    }
-    return newModel;
-  };
+  const newModel = model.toJSON();
+  debugger;
+  delete newModel.previewImageId;
+  if (newModel.previewImage) {
+    newModel.previewImage = model.previewImage.url;
+  }
+  return newModel;
+};
 
-module.exports = { previewImageToUrl };
+const imagesToUrls = (imageModels) => {
+  return imageModels.map((image) => image.url);
+};
+
+module.exports = { previewImageToUrl, imagesToUrls };
