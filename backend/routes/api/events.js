a912fa638dc0acfe11fcabaebd170caa0667aa03
fxuls <32501694+fxuls@users.noreply.github.com>
2022-03-24 20:57:18 -0400

    Add event details by id route

--- backend/routes/api/events.js
+++ backend/routes/api/events.js
@@ -2,7 +2,7 @@ const express = require("express");
 const asyncHandler = require("express-async-handler");
 
 const { Event, Group, Image, Venue } = require("../../db/models");
-const { previewImageToUrl } = require("../../utils");
+const { previewImageToUrl, imagesToUrls } = require("../../utils");
 
 const router = express.Router();
 
@@ -31,6 +31,7 @@ router.get(
   asyncHandler(async (req, res) => {
     const { groupId } = req.params;
 
+    // check that group exists
     const group = await Group.findByPk(groupId, {});
     if (!group) {
       res.status(404);
@@ -53,6 +54,39 @@ router.get(
     events = events.map(previewImageToUrl);
 
     res.json({ Events: events });
+  })
+);
+
+// get details of event by its eventId
+router.get(
+  "/events/:eventId(\\d+)",
+  asyncHandler(async (req, res) => {
+    const { eventId } = req.params;
+
+    let event = await Event.findByPk(eventId, {
+      include: [
+        { model: Group.scope(["simple", "private"]) },
+        { model: Venue.scope("excludeGroupId") },
+        { model: Image, as: "eventImages" },
+      ],
+    });
+
+    // check that event exists
+    if (!event) {
+      res.status(404);
+      return res.json({
+        message: "Event couldn't be found",
+        statusCode: 404,
+      });
+    }
+
+    // convert event to json
+    event = event.toJSON();
+
+    // convert Image models to urls
+    event.eventImages = imagesToUrls(event.eventImages);
+
+    res.json(event);
   })
 );
 
