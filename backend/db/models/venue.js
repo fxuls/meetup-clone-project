c86a0dd0e89f1bc7eab5b48c5032eeb9416cc713
fxuls <32501694+fxuls@users.noreply.github.com>
2022-03-24 20:56:54 -0400

    Add excludeGroupId scope

--- backend/db/models/venue.js
+++ backend/db/models/venue.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Venue extends Model {
     static associate(models) {
@@ -9,45 +7,53 @@ module.exports = (sequelize, DataTypes) => {
       Venue.hasMany(models.Event, { foreignKey: "venueId" });
     }
   }
-  Venue.init({
-    groupId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: { model: "Groups" },
+  Venue.init(
+    {
+      groupId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: { model: "Groups" },
+      },
+      address: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      city: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      state: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      lat: {
+        type: DataTypes.DECIMAL(6, 5),
+        allowNull: false,
+      },
+      lng: {
+        type: DataTypes.DECIMAL(6, 5),
+        allowNull: false,
+      },
     },
-    address: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    city: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    state: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    lat: {
-      type: DataTypes.DECIMAL(6,5),
-      allowNull: false,
-    },
-    lng: {
-      type: DataTypes.DECIMAL(6,5),
-      allowNull: false,
+    {
+      defaultScope: {
+        attributes: {
+          exclude: ["createdAt", "updatedAt"],
+        },
+      },
+      scopes: {
+        simple: {
+          attributes: ["id", "city", "state"],
+        },
+        excludeGroupId: {
+          attributes: {
+            exclude: ["groupId", "createdAt", "updatedAt"],
+          },
+        },
+      },
+      sequelize,
+      modelName: "Venue",
     }
-  }, {
-    defaultScope: {
-      attributes: {
-        exclude: ["createdAt", "updatedAt"]
-      }
-    },
-    scopes: {
-      simple: {
-        attributes: ["id", "city", "state"]
-      }
-    },
-    sequelize,
-    modelName: 'Venue',
-  });
+  );
   return Venue;
 };
