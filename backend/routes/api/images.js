7b4aaf34d153da427dadc35c2cc5e9f58327105c
fxuls <32501694+fxuls@users.noreply.github.com>
2022-03-26 14:44:31 -0400

    Add route to add images to a group by groupId

--- backend/routes/api/images.js
+++ backend/routes/api/images.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const asyncHandler = require("express-async-handler");
 
-const { Group, Event, Image, EventImage, Attendee, } = require("../../db/models");
-const { requireAuth, hasElevatedMembership, unauthorizedError } = require("../../utils/auth");
+const {
+  Group,
+  Event,
+  Image,
+  EventImage,
+  Attendee,
+  GroupImage
+} = require("../../db/models");
+const {
+  requireAuth,
+  hasElevatedMembership,
+  unauthorizedError,
+} = require("../../utils/auth");
 
 const router = express.Router();
 
@@ -26,24 +37,71 @@ router.post(
     }
 
     // check that user is attending the event
-    const attendance = await Attendee.findOne({ where: { userId, eventId }});
+    const attendance = await Attendee.findOne({ where: { userId, eventId } });
     if (!attendance) throw unauthorizedError();
 
     // check if image with url exists
-    let image = await Image.findOne({ where: { url }});
+    let image = await Image.findOne({ where: { url } });
     // if image does not exist create Image object
     if (!image) image = await Image.create({ url });
 
     // check if image is already added to event
-    let eventImage = await EventImage.findOne({ where: {eventId, imageId: image.id }});
+    let eventImage = await EventImage.findOne({
+      where: { eventId, imageId: image.id },
+    });
     // if eventImage does not exist create EventImage
-    if (!eventImage) eventImage = await EventImage.create({ eventId, imageId: image.id });
+    if (!eventImage)
+      eventImage = await EventImage.create({ eventId, imageId: image.id });
 
     res.json({
-        id: image.id,
-        itemId: eventId,
-        itemType: "Event",
-        url,
+      id: image.id,
+      itemId: eventId,
+      itemType: "Event",
+      url,
+    });
+  })
+);
+
+router.post(
+  "/groups/:groupId(\\d+)/images",
+  requireAuth,
+  asyncHandler(async (req, res) => {
+    const userId = req.user.id;
+    const { url } = req.body;
+    const { groupId } = req.params;
+
+    const group = await Group.findByPk(groupId);
+
+    // check that group exists
+    if (!group) {
+      res.status(404);
+      return res.json({
+        message: "Group couldn't be found",
+        statusCode: 404,
+      });
+    }
+
+    // check that user is organizer of group
+    if (userId !== group.organizerId) throw unauthorizedError();
+
+    // check if image with url exists
+    let image = await Image.findOne({ where: { url } });
+    // if image does not exist create Image object
+    if (!image) image = await Image.create({ url });
+
+    // check if image is already added to group
+    let groupImage = await GroupImage.findOne({
+      where: { groupId, imageId: image.id },
+    });
+    // if groupImage does not exist create GroupImage
+    if (!groupImage)
+      groupImage = await GroupImage.create({ groupId, imageId: image.id });
+
+    res.json({
+      id: image.id,
+      itemId: groupId,
+      itemType: "Group",
+      url,
     });
   })
 );
