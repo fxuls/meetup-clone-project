31ffe4c9ae41ed142ab3fdb9b5111380451a7fd0
fxuls <32501694+fxuls@users.noreply.github.com>
2022-05-03 20:14:48 -0400

    Add csrf handling

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,10 +5,18 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import configureStore from "./store";
+import { restoreCSRF, csrfFetch } from "./store/csrf";
+import * as sessionActions from "./store/session";
 
 const store = configureStore();
 
-if (process.env.NODE_ENV !== "production") window.store = store;
+if (process.env.NODE_ENV !== "production"){
+  restoreCSRF();
+
+  window.store = store;
+  window.csrfFetch = csrfFetch;
+  window.sessionActions = sessionActions;
+}
 
 function Root() {
   return (
