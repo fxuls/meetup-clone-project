36dc7c3aba744d46d327f4be43b7179205eea670
fxuls <32501694+fxuls@users.noreply.github.com>
2022-05-04 18:52:05 -0400

    Finish redux actions and thunks

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -5,6 +5,7 @@ export const REMOVE_USER = "session/REMOVE_USER";
 
 export const userSelector = (state) => state.session.user;
 
+// SET_USER action creator
 export function setUser(user) {
   return {
     type: SET_USER,
@@ -12,27 +13,58 @@ export function setUser(user) {
   };
 }
 
+// REMOVE_USER action creator
 export function removeUser() {
   return {
     type: REMOVE_USER,
   };
 }
 
+// login thunk
 export const login = (user) => async (dispatch) => {
-    const { email, password } = user;
-    const res = await csrfFetch("/api/session", {
-      method: "POST",
-      body: JSON.stringify({
-        email,
-        password,
-      }),
-    });
-    const data = await res.json();
-    dispatch(setUser(data.user));
-    return res;
-  };
+  const { email, password } = user;
+  const res = await csrfFetch("/api/session", {
+    method: "POST",
+    body: JSON.stringify({
+      email,
+      password,
+    }),
+  });
+  const data = await res.json();
+  dispatch(setUser(data.user));
+  return res;
+};
 
+// restore user thunk
+export const restoreUser = () => async (dispatch) => {
+  const res = await csrfFetch("/api/session");
+  const data = await res.json();
+  dispatch(setUser(data.user));
+  return res;
+};
 
+// signup thunk
+export const signup = (user) => async (dispatch) => {
+  const { firstName, lastName, email, password } = user;
+  const res = await csrfFetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      firstName,
+      lastName,
+      email,
+      password,
+    }),
+  });
+  const data = await res.json();
+  dispatch(setUser(data.user));
+  return res;
+};
+
+export const logout = () => async (dispatch) => {
+  // TODO: expire token?
+  dispatch(removeUser());
+  return;
+};
 
 export default function sessionReducer(state = { user: null }, action) {
   switch (action.type) {
