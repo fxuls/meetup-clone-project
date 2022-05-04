250460d975f16d693dbec3149e52595b04ed429a
fxuls <32501694+fxuls@users.noreply.github.com>
2022-05-04 18:52:20 -0400

    Add iniital routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,18 @@
+import { Switch, Route } from "react-router-dom";
+import LoginFormPage from "./components/LoginFormPage";
+
 function App() {
   return (
-    <h1>Hello from App</h1>
+    <>
+      <h1>Hello from App</h1>
+      <Switch>
+        <Route exact path="/">Home</Route>
+
+        <Route path="/login">
+          <LoginFormPage />
+        </Route>
+      </Switch>
+    </>
   );
 }
 
