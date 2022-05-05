767d61fdae7f677cd91da225a54cfe4a5d6cfa06
fxuls <32501694+fxuls@users.noreply.github.com>
2022-05-04 20:01:51 -0400

    Add hook to restore user on first render

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
 import { Switch, Route } from "react-router-dom";
 import LoginFormPage from "./components/LoginFormPage";
+import { useDispatch } from "react-redux";
+import { useState, useEffect } from "react";
+import { restoreUser } from "./store/session";
 
 function App() {
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  // restore user on render of App
+  useEffect(() => {
+    dispatch(restoreUser()).then(() => setIsLoaded(true));
+  }, [dispatch]);
+
   return (
     <>
       <h1>Hello from App</h1>
