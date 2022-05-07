f2ba2b71af4d56e37b9ccd1a39831d0151509511
fxuls <32501694+fxuls@users.noreply.github.com>
2022-05-07 17:55:00 -0400

    Add root div class name

--- frontend/src/components/Navigation/UnauthenticatedNavigation/UnauthenticatedNavigation.js
+++ frontend/src/components/Navigation/UnauthenticatedNavigation/UnauthenticatedNavigation.js
@@ -2,10 +2,10 @@ import { NavLink } from "react-router-dom";
 
 function UnauthenticatedNavigation() {
   return (
-    <>
+    <div id="unauth-nav">
       <NavLink to="/login">Log in</NavLink>
       <NavLink to="/signup">Sign up</NavLink>
-    </>
+    </div>
   );
 }
 
