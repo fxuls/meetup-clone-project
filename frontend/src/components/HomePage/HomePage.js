e94d20db2f9aee02263c2b69dedf092c761b01c1
fxuls <32501694+fxuls@users.noreply.github.com>
2022-05-11 21:01:55 -0400

    Add prelimiary structure

--- frontend/src/components/HomePage/HomePage.js
+++ frontend/src/components/HomePage/HomePage.js
@@ -1,9 +1,16 @@
 import "./HomePage.css";
 
 function HomePage() {
-  return <div>
-
-  </div>;
+  return (
+    <div className="home-page">
+      <div className="home-container">
+        <div className="nav-bar">
+          <button>Groups</button>
+          <button>Events</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
