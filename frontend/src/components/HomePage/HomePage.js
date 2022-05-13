3245a75089c0800ee05505a3ea7456584344211c
fxuls <32501694+fxuls@users.noreply.github.com>
2022-05-12 20:26:56 -0400

    Fetch groups and events on render on homepage

--- frontend/src/components/HomePage/HomePage.js
+++ frontend/src/components/HomePage/HomePage.js
@@ -1,6 +1,18 @@
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchGroups } from "../../store/groups";
+import { fetchEvents } from "../../store/events";
+
 import "./HomePage.css";
 
 function HomePage() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchGroups());
+    dispatch(fetchEvents());
+  }, []);
+
   return (
     <div className="home-page">
       <div className="home-container">
