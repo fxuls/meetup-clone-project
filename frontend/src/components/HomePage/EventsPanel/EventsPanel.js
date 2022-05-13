5063f32370a354aee9177c6ced34b7c479b6760a
fxuls <32501694+fxuls@users.noreply.github.com>
2022-05-12 20:57:07 -0400

    Add initial list rendering

--- frontend/src/components/HomePage/EventsPanel/EventsPanel.js
+++ frontend/src/components/HomePage/EventsPanel/EventsPanel.js
@@ -1,5 +1,14 @@
+import { useSelector } from "react-redux";
+import { allEventsSelector } from "../../../store/events";
+
 function EventsPanel () {
-    return <p>Events panel</p>;
+    const events = useSelector(allEventsSelector);
+
+    return (<div className="panel">
+        <ul className="panel-list">
+            {events.map(event => <li key={event.id}></li>)}
+        </ul>
+    </div>);
 }
 
 export default EventsPanel;
