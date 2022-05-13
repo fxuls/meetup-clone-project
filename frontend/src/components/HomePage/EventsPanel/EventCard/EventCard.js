f0552fca92e92f77a4716e6f5cb61ecfc8eaa05c
fxuls <32501694+fxuls@users.noreply.github.com>
2022-05-12 23:18:30 -0400

    Remove unnecessary container

--- frontend/src/components/HomePage/EventsPanel/EventCard/EventCard.js
+++ frontend/src/components/HomePage/EventsPanel/EventCard/EventCard.js
@@ -2,11 +2,9 @@ function EventCard({ event }) {
   const { previewImage } = event;
 
   return (
-    <div className="card-container">
-      <div className="card">
-        <img className="preview-image" src={previewImage} />
-        <p>{event.name}</p>
-      </div>
+    <div className="card">
+      <img className="preview-image" src={previewImage} />
+      <p>{event.name}</p>
     </div>
   );
 }
