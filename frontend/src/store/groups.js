cd3382e8b2247b65aa348c59851834369988212f
fxuls <32501694+fxuls@users.noreply.github.com>
2022-05-12 20:12:49 -0400

    Add fetch and set a single groups

--- frontend/src/store/groups.js
+++ frontend/src/store/groups.js
@@ -1,6 +1,7 @@
 import { csrfFetch } from "./csrf";
 
 export const SET_GROUPS = "groups/SET_GROUPS";
+export const SET_GROUP = "groups/SET_GROUP";
 
 export const allGroupsSelector = (state) => state.groups;
 
@@ -14,7 +15,15 @@ export function setGroups(groups) {
     }
 }
 
-// fetch groups thunk
+// SET_GROUP action creator
+export function setGroup(group) {
+    return {
+        type: SET_GROUP,
+        group,
+    }
+}
+
+// fetch all groups thunk
 export const fetchGroups = () => async (dispatch) => {
     const res = await csrfFetch("/api/groups");
     const data = await res.json();
@@ -22,13 +31,27 @@ export const fetchGroups = () => async (dispatch) => {
     return res;
 }
 
-export default function groupsReducer(state = { groups: {}}, action) {
+// fetch group thunk
+export const fetchGroup = (groupId) => async (dispatch) => {
+    const res = await csrfFetch(`/api/groups/${groupId}`);
+    const data = await res.json();
+    dispatch(setGroup(data));
+    return res;
+}
+
+export default function groupsReducer(state = {}, action) {
+    const newGroups = { ...state };
+
     switch (action.type) {
         case SET_GROUPS:
-            const newGroups = {};
             action.groups.forEach(group => newGroups[group.id] = group);
-            return { groups: newGroups };
+            return action.groups.reduce((newObj, group) => {
+                newObj[group.id] = group;
+                return newObj;
+            }, {});
+        case SET_GROUP:
+            newGroups[action.group.id] = action.group;
         default:
-            return { ...state };
+            return newGroups;
     }
 }
