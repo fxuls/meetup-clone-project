d85976a34b6d97148f335a9fd0b466df81360175
fxuls <32501694+fxuls@users.noreply.github.com>
2022-05-14 00:29:28 -0400

    Add realistic timestamps

--- backend/db/seeders/20220317003527-events-seed.js
+++ backend/db/seeders/20220317003527-events-seed.js
@@ -11,8 +11,8 @@ module.exports = {
        type: "inperson",
        capacity: 20,
        price: 0,
-       startDate: new Date(),
-       endDate: new Date(),
+       startDate: "2022-05-13T23:00:00.000Z",
+       endDate: "2022-05-13T03:00:00.000Z",
        previewImageId: 6,
      },
      {
@@ -20,7 +20,7 @@ module.exports = {
       name: "Virtual game night",
       description: "We will have a relaxing chill night of online gaming! We will all sign on at 6:00pm and go until about 9:00pm or whatever time everyone feels like getting off! :)",
       type: "virtual",
-      startDate: new Date(),
+      startDate: "2022-05-15T23:00:00.000Z",
       previewImageId: 8,
      },
      {
@@ -29,8 +29,8 @@ module.exports = {
       description: "Friendly tennis scrimmages in the park!",
       type: "inperson",
       venueId: 2,
-      startDate: new Date(),
-      endDate: new Date(),
+      startDate: "2022-05-13T13:00:00.000Z",
+      endDate: "2022-05-13T16:00:00.000Z",
       previewImageId: 10,
      },
      {
@@ -39,8 +39,8 @@ module.exports = {
       description: "We will be walking along the rivers! Come join us.",
       type: "inperson",
       venueId: 3,
-      startDate: new Date(),
-      endDate: new Date(),
+      startDate: "2022-05-13T11:30:00.000Z",
+      endDate: "2022-05-13T13:30:00.000Z",
       previewImageId: 11,
      },
      {
@@ -48,8 +48,8 @@ module.exports = {
       name: "Group planning meeting",
       description: "We will be meeting on Zoom in order to plan out our events for the summer.",
       type: "virtual",
-      startDate: new Date(),
-      endDate: new Date(),
+      startDate: "2022-05-16T21:00:00.000Z",
+      endDate: "2022-05-16T22:15:00.000Z",
      },
    ], {})
   },
