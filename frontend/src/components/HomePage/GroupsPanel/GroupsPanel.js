8cd17dd584b96b6bb3d4a2827aa9467e96a9761d
fxuls <32501694+fxuls@users.noreply.github.com>
2022-05-14 13:07:42 -0400

    Add html and card list

--- frontend/src/components/HomePage/GroupsPanel/GroupsPanel.js
+++ frontend/src/components/HomePage/GroupsPanel/GroupsPanel.js
@@ -1,5 +1,16 @@
+import { useSelector } from "react-redux";
+import { allGroupsSelector } from "../../../store/groups";
+import GroupCard from "./GroupCard";
+
 function GroupsPanel () {
-    return <p>Groups panel</p>;
+    const groups = Object.values(useSelector(allGroupsSelector));
+
+    return (<div className="panel">
+        <p className="sub-header">Suggested Groups for you</p>
+        <ul className="panel-list">
+            {groups.map(group => <li key={group.id}><GroupCard group={group} /></li>)}
+        </ul>
+    </div>);
 }
 
 export default GroupsPanel;
