23faf1f831f64f566491e4284d1cf1920e76c3ad
fxuls <32501694+fxuls@users.noreply.github.com>
2022-05-14 00:29:40 -0400

    Add event info to card

--- frontend/src/components/HomePage/EventsPanel/EventCard/EventCard.js
+++ frontend/src/components/HomePage/EventsPanel/EventCard/EventCard.js
@@ -1,5 +1,23 @@
+import moment from "moment";
+import { stateToAbrev } from "../../../../utils";
+
 function EventCard({ event }) {
-  const { previewImage } = event;
+  const { previewImage, Group, numAttending } = event;
+
+  // convert startDate timestamp into text format
+  // example: Fri, May 13 ⸱ 9:30 pm EDT
+  // format string: ddd, MMM D ⸱ h:mm a
+  const startDate = moment(event.startDate);
+  const startDateString = startDate.format("ddd, MMM, D • h:mm a");
+
+  // format group info
+  const groupInfo = `${Group.name} • ${Group.city}, ${stateToAbrev(
+    Group.state
+  )}`;
+
+  // format attending info
+  const attendingInfo =
+    numAttending + " " + (numAttending == 1 ? "attendee" : "attendees");
 
   return (
     <div className="card">
@@ -7,7 +25,10 @@ function EventCard({ event }) {
         <img className="preview-image" src={previewImage} />
       </div>
       <div className="card-info">
-        <p>{event.name}</p>
+        <p className="start-date">{startDateString}</p>
+        <p className="name">{event.name}</p>
+        <p className="group-info">{groupInfo}</p>
+        <p className="attending-count">{attendingInfo}</p>
       </div>
     </div>
   );
