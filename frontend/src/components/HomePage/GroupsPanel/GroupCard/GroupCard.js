8cb1cef004c7bc6ac7dbecf0b58ab2aead3a3b66
fxuls <32501694+fxuls@users.noreply.github.com>
2022-05-14 14:12:53 -0400

    Add html

--- frontend/src/components/HomePage/GroupsPanel/GroupCard/GroupCard.js
+++ frontend/src/components/HomePage/GroupsPanel/GroupCard/GroupCard.js
@@ -1,5 +1,27 @@
-function GroupCard () {
-    return null;
+import { useHistory } from "react-router-dom";
+import { stateToAbrev } from "../../../../utils";
+
+function GroupCard({ group }) {
+  const history = useHistory();
+  const { id, previewImage, name, city, state, about, numMembers, } =
+    group;
+
+  return (
+    <div
+      className="card group-card"
+      onClick={() => history.pushState(`/groups/${id}`)}
+    >
+      <div className="preview-image-container">
+        <img className="preview-image" src={previewImage} />
+      </div>
+      <div className="card-info">
+        <p className="name">{name}</p>
+        <p className="location">{`${city}, ${stateToAbrev(state)}`}</p>
+        <p className="about sub-text">{about}</p>
+        <p className="info sub-text">{`${numMembers} members • ${group.private ? "Private" : "Public"}`}</p>
+      </div>
+    </div>
+  );
 }
 
 export default GroupCard;
