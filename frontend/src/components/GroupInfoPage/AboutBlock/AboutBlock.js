5c9ae0ce5ab86fa13ab81eed98d6a3fa7b14decf
fxuls <32501694+fxuls@users.noreply.github.com>
2022-05-14 21:04:19 -0400

    Add structure

--- frontend/src/components/GroupInfoPage/AboutBlock/AboutBlock.js
+++ frontend/src/components/GroupInfoPage/AboutBlock/AboutBlock.js
@@ -1,5 +1,23 @@
-function AboutBlock () {
-    return <div className="about-block">About</div>;
+import "./AboutBlock.css";
+
+function AboutBlock({ about, organizer }) {
+  return (
+    <div className="about-block">
+      <div className="description">
+        <h2>What we're about</h2>
+        <p>{about}</p>
+      </div>
+      <div className="member-info">
+        <div className="organizer-info">
+          <h2>Organizer</h2>
+          <p>{`${organizer.firstName} ${organizer.lastName}`}</p>
+        </div>
+        <ul className="member-list">
+          <h2>Members</h2>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default AboutBlock;
