bca49e961e5d7e06f0b66ff798bb63cfa372c4d1
fxuls <32501694+fxuls@users.noreply.github.com>
2022-05-14 21:31:19 -0400

    Add hover shadow to button

--- frontend/src/components/GroupInfoPage/PrivateGroupBlock/PrivateGroupBlock.js
+++ frontend/src/components/GroupInfoPage/PrivateGroupBlock/PrivateGroupBlock.js
@@ -9,7 +9,7 @@ function PrivateGroupBlock() {
     <div className="private-block">
       <h2>This group is private</h2>
       <p>You must be a member in order to view the contents of this group.</p>
-      <button onClick={reqToJoinClicked}>Request to Join</button>
+      <button className="hover-shadow" onClick={reqToJoinClicked}>Request to Join</button>
     </div>
   );
 }
