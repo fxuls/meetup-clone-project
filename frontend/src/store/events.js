a928da99319b81bcbcb1a403e66da46d7754f738
fxuls <32501694+fxuls@users.noreply.github.com>
2022-05-15 17:08:17 -0400

    Add fetching attendees of an event

--- frontend/src/store/events.js
+++ frontend/src/store/events.js
@@ -2,10 +2,11 @@ import { csrfFetch } from "./csrf";
 
 export const SET_EVENTS = "events/SET_EVENTS";
 export const SET_EVENT = "events/SET_EVENT";
+export const SET_ATTENDEES = "events/SET_ATTENDEES";
 
 export const allEventsSelector = (state) => state.events;
-
 export const eventSelector = (eventId) => (state) => state.events[eventId];
+export const attendeesSelector = (eventId) => (state) => state.events[eventId]?.attendees;
 
 // SET_EVENT action creator
 export function setEvents(events) {
@@ -20,6 +21,15 @@ export function setEvent(event) {
     return {
         type: SET_EVENT,
         event,
+    }
+}
+
+// SET_ATTENDEES action creator
+export function setAttendees(evnetId, attendees) {
+    return {
+        type: SET_ATTENDEES,
+        eventId,
+        attendees,
     }
 }
 
@@ -39,6 +49,18 @@ export const fetchEvent = (eventId) => async (dispatch) => {
     return res;
 }
 
+// fetch attendees thunk
+export const fetchAttendees = (eventId) => async (dispatch) => {
+    const res = await csrfFetch(`/api/events/${eventId}/attendees`);
+    const data = await res.json();
+
+    const attendeesObj = {};
+    data.Attendees.forEach((attendee) => attendeesObj[attendee.id] = attendee);
+
+    dispatch(setAttendees(eventId, attendeesObj));
+    return res;
+}
+
 export default function eventsReducer(state = {}, action) {
     const newEvents = { ...state };
 
@@ -50,6 +72,11 @@ export default function eventsReducer(state = {}, action) {
             }, {});
         case SET_EVENT:
             newEvents[action.event.id] = action.event;
+            break;
+        case SET_ATTENDEES:
+            const { attendees, eventId } = action;
+            if (newEvents[eventId]) newEvents[eventId].attendees = attendees;
+            else newEvents[eventId] = { attendees };
             break;
     }
 
