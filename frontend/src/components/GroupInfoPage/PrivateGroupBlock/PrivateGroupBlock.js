4fd42cda21cd267e50aab556fde2c09b1dc6938f
fxuls <32501694+fxuls@users.noreply.github.com>
2022-05-14 23:51:18 -0400

    Add functionality for request join button

--- frontend/src/components/GroupInfoPage/PrivateGroupBlock/PrivateGroupBlock.js
+++ frontend/src/components/GroupInfoPage/PrivateGroupBlock/PrivateGroupBlock.js
@@ -1,15 +1,48 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { userSelector } from "../../../store/session";
+import { csrfFetch } from "../../../store/csrf";
+
 import "./PrivateGroupBlock.css";
 
-function PrivateGroupBlock() {
-  const reqToJoinClicked = () => {
-    // TODO add functionality for button
+function PrivateGroupBlock({ groupId }) {
+  const history = useHistory();
+
+  const [hasRequestedJoin, setHasRequestedJoin] = useState(false);
+  const [requestedFeedback, setRequestedFeedback] = useState("Thanks, you'll hear from the organizer soon");
+
+  const user = useSelector(userSelector);
+
+  const reqToJoinClicked = async () => {
+    // if user is logged in send request for membership
+    if (user) {
+      const res = await csrfFetch(`/api/groups/${groupId}/join`, {
+        method: "POST",
+      });
+      const data = await res.json();
+
+      setHasRequestedJoin(true);
+      if (res.statusCode !== 200) setRequestedFeedback(data.message);
+    }
+
+    // if not logged in send user to login page
+    else {
+      history.push("/login");
+    }
   };
 
   return (
     <div className="private-block">
       <h2>This group is private</h2>
       <p>You must be a member in order to view the contents of this group.</p>
-      <button className="hover-shadow" onClick={reqToJoinClicked}>Request to Join</button>
+      {hasRequestedJoin ? (
+        <p className="requested-feedback">{requestedFeedback}</p>
+      ) : (
+        <button className="hover-shadow" onClick={reqToJoinClicked}>
+          Request to Join
+        </button>
+      )}
     </div>
   );
 }
