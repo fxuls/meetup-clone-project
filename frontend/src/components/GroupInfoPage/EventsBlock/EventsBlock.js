7e3795094b4488e740850918e0a53e62eb23d684
fxuls <32501694+fxuls@users.noreply.github.com>
2022-05-15 03:32:36 -0400

    Add EventCards for each event

--- frontend/src/components/GroupInfoPage/EventsBlock/EventsBlock.js
+++ frontend/src/components/GroupInfoPage/EventsBlock/EventsBlock.js
@@ -1,5 +1,17 @@
-function EventsBlock () {
-    return <div className="events-block">Events</div>;
+import EventCard from "../../HomePage/EventsPanel/EventCard";
+
+import "./EventsBlock.css";
+
+function EventsBlock({ events }) {
+  return (
+    <div className="events-block">
+      <ul className="events-list">
+          {Object.values(events).map((event) => (<li key={event.id}>
+            <EventCard event={event} />
+          </li>))}
+      </ul>
+    </div>
+  );
 }
 
 export default EventsBlock;
