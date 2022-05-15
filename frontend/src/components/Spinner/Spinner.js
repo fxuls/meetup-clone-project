356d5fa39111257febff456bb10b38b65f273584
fxuls <32501694+fxuls@users.noreply.github.com>
2022-05-14 23:31:01 -0400

    Add a spinner wrapper

--- frontend/src/components/Spinner/Spinner.js
+++ frontend/src/components/Spinner/Spinner.js
@@ -1,5 +1,5 @@
 function Spinner () {
-    return <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div>;
+    return <div className="spinner"><div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div></div>;
 }
 
 export default Spinner;
