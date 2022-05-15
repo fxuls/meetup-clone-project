5a032ea95df83e96bba3fc712c6170c457ada3f7
fxuls <32501694+fxuls@users.noreply.github.com>
2022-05-14 21:23:48 -0400

    Add more info and a button

--- frontend/src/components/GroupInfoPage/PrivateGroupBlock/PrivateGroupBlock.js
+++ frontend/src/components/GroupInfoPage/PrivateGroupBlock/PrivateGroupBlock.js
@@ -1,5 +1,17 @@
-function PrivateGroupBlock () {
-    return <div>This groups is private</div>
+import "./PrivateGroupBlock.css";
+
+function PrivateGroupBlock() {
+  const reqToJoinClicked = () => {
+    // TODO add functionality for button
+  };
+
+  return (
+    <div className="private-block">
+      <h2>This group is private</h2>
+      <p>You must be a member in order to view the contents of this group.</p>
+      <button onClick={reqToJoinClicked}>Request to Join</button>
+    </div>
+  );
 }
 
 export default PrivateGroupBlock;
