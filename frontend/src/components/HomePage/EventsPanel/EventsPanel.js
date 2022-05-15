6abfcd6d8576033b3f22c850d4da0bb0fc19b2ad
fxuls <32501694+fxuls@users.noreply.github.com>
2022-05-15 06:44:52 -0400

    Update formatting

--- frontend/src/components/HomePage/EventsPanel/EventsPanel.js
+++ frontend/src/components/HomePage/EventsPanel/EventsPanel.js
@@ -2,15 +2,21 @@ import { useSelector } from "react-redux";
 import { allEventsSelector } from "../../../store/events";
 import EventCard from "./EventCard/EventCard";
 
-function EventsPanel () {
-    const events = Object.values(useSelector(allEventsSelector));
+function EventsPanel() {
+  const events = Object.values(useSelector(allEventsSelector));
 
-    return (<div className="panel">
-        <p className="sub-header">Suggested Events for you</p>
-        <ul className="panel-list">
-            {events.map(event => <li key={event.id}><EventCard event={event} /></li>)}
-        </ul>
-    </div>);
+  return (
+    <div className="panel">
+      <p className="sub-header">Suggested Events for you</p>
+      <ul className="panel-list">
+        {events.map((event) => (
+          <li key={event.id}>
+            <EventCard event={event} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default EventsPanel;
