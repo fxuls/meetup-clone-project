b707d1720d154126780058875536c33a12bc7f08
fxuls <32501694+fxuls@users.noreply.github.com>
2022-05-16 16:45:46 -0400

    Add create event thunk

--- frontend/src/store/events.js
+++ frontend/src/store/events.js
@@ -6,84 +6,118 @@ export const SET_ATTENDEES = "events/SET_ATTENDEES";
 
 export const allEventsSelector = (state) => state.events;
 export const eventSelector = (eventId) => (state) => state.events[eventId];
-export const attendeesSelector = (eventId) => (state) => state.events[eventId]?.attendees;
+export const attendeesSelector = (eventId) => (state) =>
+  state.events[eventId]?.attendees;
 
 // SET_EVENT action creator
 export function setEvents(events) {
-    return {
-        type: SET_EVENTS,
-        events,
-    }
+  return {
+    type: SET_EVENTS,
+    events,
+  };
 }
 
 // SET_EVENT action creator
 export function setEvent(event) {
-    return {
-        type: SET_EVENT,
-        event,
-    }
+  return {
+    type: SET_EVENT,
+    event,
+  };
 }
 
 // SET_ATTENDEES action creator
 export function setAttendees(eventId, attendees) {
-    return {
-        type: SET_ATTENDEES,
-        eventId,
-        attendees,
-    }
+  return {
+    type: SET_ATTENDEES,
+    eventId,
+    attendees,
+  };
 }
 
 // fetch all events thunk
 export const fetchEvents = () => async (dispatch) => {
-    const res = await csrfFetch("/api/events");
-    const data = await res.json();
-    dispatch(setEvents(data.Events));
-    return res;
-}
+  const res = await csrfFetch("/api/events");
+  const data = await res.json();
+  dispatch(setEvents(data.Events));
+  return res;
+};
 
 // fetch event thunk
 export const fetchEvent = (eventId) => async (dispatch) => {
-    const res = await csrfFetch(`/api/events/${eventId}`);
-    const data = await res.json();
-    dispatch(setEvent(data));
-    return res;
-}
+  const res = await csrfFetch(`/api/events/${eventId}`);
+  const data = await res.json();
+  dispatch(setEvent(data));
+  return res;
+};
 
 // fetch attendees thunk
 export const fetchAttendees = (eventId) => async (dispatch) => {
-    const res = await csrfFetch(`/api/events/${eventId}/attendees`);
-    const data = await res.json();
+  const res = await csrfFetch(`/api/events/${eventId}/attendees`);
+  const data = await res.json();
 
-    const attendeesObj = {};
-    data.Attendees.forEach((attendee) => attendeesObj[attendee.id] = {
+  const attendeesObj = {};
+  data.Attendees.forEach(
+    (attendee) =>
+      (attendeesObj[attendee.id] = {
         id: attendee.id,
         firstName: attendee.firstName,
         lastName: attendee.lastName,
         status: attendee.Attendance.status,
-    });
+      })
+  );
 
-    dispatch(setAttendees(eventId, attendeesObj));
-    return res;
-}
+  dispatch(setAttendees(eventId, attendeesObj));
+  return res;
+};
+
+// create event thunk
+export const createEvent = (event) => async (dispatch) => {
+  const {
+    name,
+    groupId,
+    description,
+    type,
+    capacity,
+    price,
+    startDate,
+    endDate,
+  } = event;
+
+  const res = await csrfFetch(`/api/groups/${groupId}/events`, {
+    method: "POST",
+    body: JSON.stringify({
+      venueId: 1, // TODO make this chosen by user instead of preset
+      name,
+      description,
+      type,
+      capacity,
+      price,
+      startDate,
+      endDate,
+    }),
+  });
+  const data = await res.json();
+  return res;
+};
 
 export default function eventsReducer(state = {}, action) {
-    const newEvents = { ...state };
+  const newEvents = { ...state };
 
-    switch (action.type) {
-        case SET_EVENTS:
-            return action.events.reduce((newObj, event) => {
-                newObj[event.id] = event;
-                return newObj;
-            }, {});
-        case SET_EVENT:
-            newEvents[action.event.id] = action.event;
-            break;
-        case SET_ATTENDEES:
-            const { attendees, eventId } = action;
-            if (newEvents[eventId]) newEvents[eventId].attendees = attendees;
-            else newEvents[eventId] = { attendees };
-            break;
-    }
+  switch (action.type) {
+    case SET_EVENTS:
+      return action.events.reduce((newObj, event) => {
+        newObj[event.id] = event;
+        return newObj;
+      }, {});
+    case SET_EVENT:
+      newEvents[action.event.id] = action.event;
+      break;
+    case SET_ATTENDEES:
+      const { attendees, eventId } = action;
+      if (newEvents[eventId]) newEvents[eventId].attendees = attendees;
+      else newEvents[eventId] = { attendees };
+      break;
+  }
 
-    return newEvents;
+  return newEvents;
 }
